2df5153855251f810a5978530e6eefeb175f63b3
Prathima NM <T6ST@eurofins.com>
2021-07-12 19:28:53 +0530

    checkout master to develop branch

--- .scripts/install-packages.ps1
+++ .scripts/install-packages.ps1
@@ -21,16 +21,23 @@ $NuGetSource = "https://nuget.pkg.github.com/prathimanm/index.json"
 
 dotnet build -c Release
 
-Function Publish-Package {
+Function Publish-Package-Tool {
     Param ([string]$Folder, [string]$Name)
     Write-Host -ForegroundColor Magenta "Packing and publishing $Folder-$Name package"
     dotnet pack ./$Folder/$Name/$Name.csproj -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg -c Release-Tool /p:Version=$Version
     dotnet nuget push ./$Folder/$Name/bin/Release-Tool/dotnet-nswagen.$Version.nupkg -s $NuGetSource --api-key $ApiKey
 }
 
+Function Publish-Package {
+    Param ([string]$Folder, [string]$Name)
+    Write-Host -ForegroundColor Magenta "Packing and publishing $Folder-$Name package"
+    dotnet pack ./$Folder/$Name/$Name.csproj -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg -c Release /p:Version=$Version
+    dotnet nuget push ./$Folder/$Name/bin/Release/dotnet-nswagen.$Version.nupkg -s $NuGetSource --api-key $ApiKey
+}
+
+
    #Publish-Package "src" "NSwagen.Annotations"
-   #Publish-Package "src" "NSwagen.Core"
-   Publish-Package "src" "NSwagen"
+   Publish-Package-Tool "src" "NSwagen"
    
    
    
