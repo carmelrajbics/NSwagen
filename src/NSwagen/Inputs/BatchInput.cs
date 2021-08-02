f03ab98fb5e02f0abe697d82c9332032a6cc8628
Prathima NM <T6ST@eurofins.com>
2021-08-02 16:28:04 +0530

    Command description changes Included nswag typescript nuget package reference.

--- src/NSwagen/Inputs/BatchInput.cs
+++ src/NSwagen/Inputs/BatchInput.cs
@@ -4,7 +4,7 @@ namespace NSwagen.Cli.Inputs
 {
     public class BatchInput
     {
-        [Description("The configuration file defining the route information.", Name = "config")]
+        [Description("The configuration file defining the multiple client generator information.", Name = "config")]
         public string ConfigurationFile { get; set; } = null!;
     }
 }
